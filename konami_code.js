7b3aae41ac3a4b33a29538e688b78e95f8349aaa
Cassandra Corrales <cmcorrales3@gmail.com>
2018-05-14 02:01:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,18 +12,19 @@ const codes = [
 ];
 
 function init() {
+  let body = document.querySelector("body")
   let i = 0;
-  document.body.addEventListener('keydown', function(e) {
+  body.addEventListener('keydown', function(e) {
   let keyPressed = e.key;  
-  if (keyPressed.toString() == codes[i]) {
-    i++;
-    if (i === codes.length-1) {
-      alert("Hurray!");
+      if (keyPressed.toString() == codes[i]) {
+        i++;
+      }
+      else {
+        i = 0;
+      }
+  if (i == codes.length) {
+      alert("WOO KONAMI CODE");
       i=0;
-    }
-  }
-   else {
-    i = 0;
   }
   });
 }
